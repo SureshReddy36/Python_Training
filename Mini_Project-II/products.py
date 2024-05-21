594311d136c09a73f91300a44f6bcccb3ae3f76e
SureshReddy36 <118824237+SureshReddy36@users.noreply.github.com>
2024-05-21 23:21:57 +0530

    Add files via upload

---
+++ Mini_Project-II/products.py
@@ -0,0 +1,136 @@
+class hi:
+    def mobile(self):
+        self.cost=10000
+        self.stock=100
+        self.name="mobile"
+        print("The price of the mobile is",self.cost)
+        print("press 1 for buy and 2 for no")
+        bn=int(input())
+        if bn==1:
+            self.stock-=1
+            print("Stock remaining:",self.stock)
+            print("Thanks for buying our product")
+            print("pay",self.cost,"while receiving the order")
+        else:
+            print("Thank for visting us")
+    def laptop(self):
+        self.cost=50000
+        self.stock=1000
+        self.name="Laptop"
+        print("The price of the laptop is",self.cost)
+        print("press 1 for buy and 2 for no")
+        bn=int(input())
+        if bn==1:
+            self.stock-=1
+            print("Stock remaining:",self.stock)
+            print("Thanks for buying our product")
+            print("pay",self.cost,"while receiving the order")
+        else:
+            print("Thank for visting us")
+    def watch(self):
+        self.cost=2500
+        self.stock=10000
+        self.name="watch"
+        print("The price of the watch is",self.cost)
+        print("press 1 for buy and 2 for no")
+        bn=int(input())
+        if bn==1:
+            self.stock-=1
+            print("Stock remaining:",self.stock)
+            print("Thanks for buying our product")
+            print("pay",self.cost,"while receiving the order")
+        else:
+            print("Thank for visting us")
+#accessories
+    def earphones(self):
+        self.cost=1000
+        self.stock=1000
+        self.name="earphones"
+        print("The price of the earphones is",self.cost)
+        print("press 1 for buy and 2 for no")
+        bn=int(input())
+        if bn==1:
+            self.stock-=1
+            print("Stock remaining:",self.stock)
+            print("Thanks for buying our product")
+            print("pay",self.cost,"while receiving the order")
+        else:
+            print("Thank for visting us")
+    def cases(self):
+        self.cost=200
+        self.stock=10000
+        self.name="cases"
+        print("The price of the cases is",self.cost)
+        print("press 1 for buy and 2 for no")
+        bn=int(input())
+        if bn==1:
+            self.stock-=1
+            print("Stock remaining:",self.stock)
+            print("Thanks for buying our product")
+            print("pay",self.cost,"while receiving the order")
+        else:
+            print("Thank for visting us")
+    def charger(self):
+        self.cost=2500
+        self.stock=10000
+        self.name="watch"
+        print("The price of the charger is",self.cost)
+        print("press 1 for buy and 2 for no")
+        bn=int(input())
+        if bn==1:
+            self.stock-=1
+            print("Stock remaining:",self.stock)
+            print("Thanks for buying our product")
+            print("pay",self.cost,"while receiving the order")
+        else:
+            print("Thank for visting us")
+#clothing
+    def shirts(self):
+        self.cost=1000
+        self.stock=1000
+        self.name="shirts"
+        print("The price of the shirt is",self.cost)
+        print("press 1 for buy and 2 for no")
+        bn=int(input())
+        if bn==1:
+            self.stock-=1
+            print("Stock remaining:",self.stock)
+            print("Thanks for buying our product")
+            print("pay",self.cost,"while receiving the order")
+        else:
+            print("Thank for visting us")
+    def pants(self):
+        self.cost=1000
+        self.stock=10000
+        self.name="pantd"
+        print("The price of the pants is",self.cost)
+        print("press 1 for buy and 2 for no")
+        bn=int(input())
+        if bn==1:
+            self.stock-=1
+            print("Stock remaining:",self.stock)
+            print("Thanks for buying our product")
+            print("pay",self.cost,"while receiving the order")
+        else:
+            print("Thank for visting us")
+    def trousers(self):
+        self.cost=2000
+        self.stock=10000
+        self.name="trousers"
+        print("The price of the trousers is",self.cost)
+        print("press 1 for buy and 2 for no")
+        bn=int(input())
+        if bn==1:
+            self.stock-=1
+            print("Stock remaining:",self.stock)
+            print("Thanks for buying our product")
+            print("pay",self.cost,"while receiving the order")
+        else:
+            print("Thank for visting us")
+
+        
+    
+             
+             
+        
+   
